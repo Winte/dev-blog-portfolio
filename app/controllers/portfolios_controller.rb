efebccd05307104243a5db84d89fa7ad2e76baed
Jakub Vintera <jakubvintera@MacBook-Pro.local>
2019-03-15 14:45:39 +0100

    Updated sort action with json for the error in console

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,7 +12,7 @@ class PortfoliosController < ApplicationController
       Portfolio.find(value[:id]).update(position: value[:position])
     end
 
-    render nothing: true
+    render json: { data: @portfolio_item }
   end
 
 
