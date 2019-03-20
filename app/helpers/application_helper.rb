20e952796d5614b9dec587cf82c239990a3ab231
Jakub Vintera <77jonydavid@gmail.com>
2019-03-20 07:04:17 +0100

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -62,4 +62,23 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+      alert = (flash[:alert] || flash[:error] || flash[:notice])
+  
+      if alert
+        alert_generator alert
+      end
+  end
+  
+  def alert_generator msg
+      js add_gritter(msg, title: "Spiriqual Portfolio", sticky: false)
+  end
+
+
+
+
+
+
+
+
 end
