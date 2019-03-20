5415076659edadb5b884b10ac354b404f3b1de28
Jakub Vintera <77jonydavid@gmail.com>
2019-03-20 17:50:49 +0100

    Implemented jquery functions for action cable comment feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.build(comment_paramas)
-  end
-
-  private
-
-  def comment_paramas
-    params.require(:comment).permit(:content)
+      @comment = current_user.comments.build(comment_params)
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:content)
   end
 end
